f0c108f2b1e3bc8cea699b7f8cffa9acc58a6281
aa <Nahida-aa@outlook.com>
2026-04-27 23:50:13 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,63 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			      title: {
-        en: 'My Docs',
-        'zh-CN': '我的文档',
-      },
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: {
+				en: 'My Docs',
+				'zh-CN': '我的文档',
+			},
+			customCss: [
+				// Path to your Tailwind base styles:
+				'./src/styles/global.css',
+			],
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/xia-aa' }],
 			sidebar: [
 				{
 					label: 'Guides',
+					translations: {
+						"zh-CN": "指南",
+					},
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{
+							label: 'Example Guide',
+							translations: {
+								"zh-CN": "示例指南",
+							},
+							slug: 'guides/example'
+						},
 					],
 				},
 				{
 					label: 'Reference',
+					translations: {
+						"zh-CN": "参考",
+					},
 					autogenerate: { directory: 'reference' },
 				},
 			],
-			      // 为此网站设置英语为默认语言。
-      defaultLocale: 'root',
-      locales: {
-		root: {
-          label: 'English',
-          lang: 'en', // lang 是 root 语言必须的
-        },
-        // 简体中文文档在 `src/content/docs/zh-cn/` 中。
-        'zh-cn': {
-          label: '简体中文',
-          lang: 'zh-CN',
-        },
-      },
+			// 为此网站设置英语为默认语言。
+			defaultLocale: 'root',
+			locales: {
+				root: {
+					label: 'English',
+					lang: 'en', // lang 是 root 语言必须的
+				},
+				// 简体中文文档在 `src/content/docs/zh-cn/` 中。
+				'zh-cn': {
+					label: '简体中文',
+					lang: 'zh-CN',
+				},
+			},
 		}),
 	],
+
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
---
+++ src/content/docs/zh-cn/guides/example.md
@@ -0,0 +1,11 @@
+---
+title: 示例指南
+description: 我新的 Starlight 文档站点中的指南。
+---
+
+指南引导用户完成他们想要完成的特定任务，通常包含一系列步骤。
+编写好的指南需要考虑用户尝试做的事情。
+
+## 进一步阅读
+
+- 在 Diátaxis 框架中阅读[关于操作指南](https://diataxis.fr/how-to-guides/)
